27c5ebd275d402c30f69e29d5e962d3c586517f2
milovidova <145771799+milovidova@users.noreply.github.com>
2025-03-26 17:19:08 +0300

    newww

--- webpack.common.js
+++ webpack.common.js
@@ -131,6 +131,163 @@ module.exports = {
       filename: './styleguide.html'
     }),
 
+    // Страница с новыми статьями
+    new HtmlWebpackPlugin({
+      template: './src/newarticles.html',
+      filename: './newarticles.html'
+    }),
+
+    // Страница с новыми тестами
+    new HtmlWebpackPlugin({
+      template: './src/newtest.html',
+      filename: './newtest.html'
+    }),
+    // Страница с новой терапией
+    new HtmlWebpackPlugin({
+      template: './src/newtherapy.html',
+      filename: './newtherapy.html'
+    }),
+
+    // Страница с новыми мыслями
+    new HtmlWebpackPlugin({
+      template: './src/newthoughts.html',
+      filename: './newthoughts.html'
+    }),
+    // Страницы со статьями:
+    new HtmlWebpackPlugin({
+      template: './src/new_articles/article1.html',
+      filename: './article1.html'
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/new_articles/article2.html',
+      filename: './article2.html'
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/new_articles/article3.html',
+      filename: './article3.html'
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/new_articles/article4.html',
+      filename: './article4.html'
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/new_articles/article5.html',
+      filename: './article5.html'
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/new_articles/article6.html',
+      filename: './article6.html'
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/new_articles/article7.html',
+      filename: './article7.html'
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/new_articles/article8.html',
+      filename: './article8.html'
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/new_articles/article9.html',
+      filename: './article9.html'
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/new_articles/article10.html',
+      filename: './article10.html'
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/new_articles/article11.html',
+      filename: './article11.html'
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/new_articles/article12.html',
+      filename: './article12.html'
+    }),
+    // дни
+    new HtmlWebpackPlugin({
+      template: './src/newdays/day1.html',
+      filename: './day1.html'
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/newdays/day2.html',
+      filename: './day2.html'
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/newdays/day3.html',
+      filename: './day3.html'
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/newdays/day4.html',
+      filename: './day4.html'
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/newdays/day5.html',
+      filename: './day5.html'
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/newdays/day6.html',
+      filename: './day6.html'
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/newdays/day7.html',
+      filename: './day7.html'
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/newdays/day8.html',
+      filename: './day8.html'
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/newdays/day9.html',
+      filename: './day9.html'
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/newdays/day10.html',
+      filename: './day10.html'
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/newdays/day11.html',
+      filename: './day11.html'
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/newdays/day12.html',
+      filename: './day12.html'
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/newdays/day13.html',
+      filename: './day13.html'
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/newdays/day14.html',
+      filename: './day14.html'
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/newdays/day15.html',
+      filename: './day15.html'
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/newdays/day16.html',
+      filename: './day16.html'
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/newdays/day17.html',
+      filename: './day17.html'
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/newdays/day18.html',
+      filename: './day18.html'
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/newdays/day19.html',
+      filename: './day19.html'
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/newdays/day20.html',
+      filename: './day20.html'
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/newdays/day21.html',
+      filename: './day21.html'
+    }),
+
     // Internal pages
     // new HtmlWebpackPlugin({
     //   hash: true,
